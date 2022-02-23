5110cb51c473aaf4ffe2aff0890b2de506b5a146
mona <csimonusa@outlook.com>
2022-02-23 16:37:47 -0500

    structure for user controller

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const {User, Thought} = require('../models')
+
+module.exports = {
+
+    getUsers(res,res) {
+
+    },
+
+    getSingleUser(req, res) {
+
+    },
+
+    createUser(res,res) {
+
+    },
+
+    updateUser(res,res) {
+
+    },
+
+    deleteUser(res,res) {
+
+    },
+
+    createUserFriends(res,res) {
+
+    },
+
+    deleteUserFriends(res,res) {
+
+    },
+}
