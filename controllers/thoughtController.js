84beed89d33571057a20f31acc708760bdf68686
mona <csimonusa@outlook.com>
2022-02-23 16:39:24 -0500

    copied and refactored user controller for thought

---
+++ controllers/thoughtController.js
@@ -0,0 +1,32 @@
+const {Thought, User} = require('../models')
+
+module.exports = {
+
+    getThoughts(res,res) {
+
+    },
+
+    getSingleThought(req, res) {
+
+    },
+
+    createThought(res,res) {
+
+    },
+
+    updateThought(res,res) {
+
+    },
+
+    deleteThought(res,res) {
+
+    },
+
+    createReaction(res,res) {
+
+    },
+
+    deleteReaction(res,res) {
+
+    },
+}
