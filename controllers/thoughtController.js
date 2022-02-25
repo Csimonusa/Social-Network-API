9a6a6f59645ffd2a95edd96574146dc633230893
mona <csimonusa@outlook.com>
2022-02-25 17:53:49 -0500

    create thought route

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,15 +3,24 @@ const {Thought, User} = require('../models')
 module.exports = {
 
     getThoughts(req,res) {
-
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.json(err))
     },
 
     getSingleThought(req, res) {
-
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then((thought) => !thought ? res.json({ message: 'No thought with that ID '}) : res.json(thought))
     },
 
     createThought(req,res) {
-
+        Thought.create(req.body)
+            .then((thought) => User.findOneAndUpdate(
+                { username: thought.username },
+                {$addToSet: { thoughts: thought._id }},
+                { new: true }))
+            .then((thought) => !thought ? res.json({ message: 'No thought with that ID '}) : res.json(thought))
+            
     },
 
     updateThought(req,res) {
